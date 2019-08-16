4595ac55d969f84f4d04c9e4ef140f9e6a876430
rinti <andreas.bernacca@frojd.se>
2019-08-16 09:49:57 +0200

    Use realtime api instead

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import os
-import datetime
 
 from dotenv import load_dotenv
 import httpx
@@ -8,39 +7,24 @@ import pprint
 load_dotenv()
 
 API_KEY = os.environ["TRAFIKLAB_API_KEY"]
-URL = "https://api.resrobot.se/v2/trip.json?key={}&originId={}&destId={}&passlist=0&date={}&time={}&products=128"
+URL = "http://api.sl.se/api2/realtimedeparturesv4.json?key={}&siteid={}&timewindow=60&metro=false&train=false&tram=false&ship=false"
 
-# 740046158 = Rosenlundsgatan
-# 740021705 = Gullmars
-# 740053057 = Årsta torg
 
 PAIRS = (("740046158", "740021705"), ("740053057", "740021705"))
 
-stops = []
+in_data = {1367: {"name": "Rosenlundsgatan"}, 1500: {"name": "Årsta torg"}}
 
-for pair in PAIRS:
-    now = datetime.datetime.now()
-    today = str(now.date())
-    time = "{}:{:02d}".format(now.hour, now.minute)
+data = []
 
-    origin, destination = pair
-    url = URL.format(API_KEY, origin, destination, today, time)
-    req = httpx.get(url)
+for x in in_data.keys():
+    req = httpx.get(URL.format(API_KEY, x))
     json = req.json()
 
-    name = req.json()["Trip"][0]["LegList"]["Leg"][0]["Origin"]["name"]
-    times = []
-
-    for trip in json["Trip"]:
-        times.append(
-            {
-                "from": trip["LegList"]["Leg"][0]["Origin"]["name"],
-                "with": trip["LegList"]["Leg"][0]["Product"]["name"],
-                "to": trip["LegList"]["Leg"][0]["Destination"]["name"],
-                "time": trip["LegList"]["Leg"][0]["Origin"]["time"],
-            }
+    trips = list(
+        filter(
+            lambda x: x["Destination"] == "Gullmarsplan", json["ResponseData"]["Buses"]
         )
+    )
+    data.append({"name": in_data[x]["name"], "trips": trips})
 
-    stops.append({"name": name, "times": times})
-
-pprint.pprint(stops)
+pprint.pprint(data)
